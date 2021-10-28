fab24ebcfc1290927a542bbbefdc3f86bf22efbb
Zack Steinkamp <zack@steinkamp.us>
2021-10-27 22:09:32 -0700

    fix some math

--- src/app.js
+++ src/app.js
@@ -20,14 +20,14 @@ const refreshState = (myQ) => {
         console.warn('WEIRD: garagedoor does not have state. Device:', device);
         continue;
       }
-      if (device.state.door_state === 'closed') {
+      if (device.state.door_state !== 'closed') {
         if (!openDoors[device.serial_number]) {
           // not already known as open
           console.info('Garage door ', device.serial_number, ' is now open.');
           openDoors[device.serial_number] = { openAt: new Date() };
         } else {
           // known as open already
-          const openAgeMinutes = (new Date()) - openDoors[device.serial_number].openAt / 60 / 1000;
+          const openAgeMinutes = Math.round(((new Date()) - openDoors[device.serial_number].openAt) / 60 / 1000);
           console.info('Garage door ', device.serial_number, ' has been open for ', openAgeMinutes, ' minutes.');
           if (openAgeMinutes > (process.env.MYQ_MAX_OPEN_DURATION_MINUTES || 10)) {
             console.error('THIS IS A PROBLEM!');
