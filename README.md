75b33af8a95cb8a5367ae92fe00c8da535c878fe
Zack Steinkamp <zack@steinkamp.us>
2021-10-27 21:19:12 -0700

    use myq library; working test!

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Monitor your myQ supported garage door (Chamberlain, Liftmaster) status and send you a notification if it was open more than N minutes.
 
+You will need to set the environment variables `MYQ_USER` and `MYQ_PASS` before running the commands below.
+
 ## Running
 ```
 docker compose run app
--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const index = require('./index.js');
+const MQ = require('@hjdhjd/myq').myQApi;
 
-console.log(index);
+if (!process.env.MYQ_USER || !process.env.MYQ_PASS) {
+  console.log('ERROR: Env vars MYQ_USER and/or MYQ_PASS are not defined!');
+  process.exit(1);
+}
+
+const myQ = new MQ(process.env.MYQ_USER, process.env.MYQ_PASS);
+myQ.refreshDevices().then(() => {
+  console.log(myQ.devices);
+});
