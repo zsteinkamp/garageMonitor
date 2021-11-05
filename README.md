08186d708c705386886125bb6b1f45ae5309a80d
Zack Steinkamp <zack@steinkamp.us>
2021-11-04 20:39:49 -0700

    send mail

--- README.md
+++ README.md
@@ -2,7 +2,22 @@
 
 Monitor your myQ supported garage door (Chamberlain, Liftmaster) status and send you a notification if it was open more than N minutes.
 
-You will need to set the environment variables `MYQ_USER` and `MYQ_PASS` before running the commands below.
+You will need to set the following environment variables before running the commands below. You can paste the content below into a file called `.env` in the current directory if you like.
+
+```
+MYQ_USER="you@yourself.com"
+MYQ_PASS="myqpassword"
+SMTP_USER="smtpuser"
+SMTP_PASS="smtppass"
+SMTP_HOST="smtp.some.server"
+SMTP_PORT=587
+SMTP_FROM="you@yourself.com"
+SMTP_TO="you@yourself.com"
+```
+
+You may want to use your text messaging email address for the `SMTP_TO`
+variable. Poor man's push notifications. For example, if you are with AT&T,
+then your text message email address is `{yourphonenumber}@txt.att.net`.
 
 ## Running
 ```
--- src/app.js
+++ src/app.js
@@ -1,15 +1,36 @@
 'use strict';
 
 const MQ = require('@hjdhjd/myq').myQApi;
+const nodemailer = require('nodemailer');
 
-if (!process.env.MYQ_USER || !process.env.MYQ_PASS) {
-  console.log('ERROR: Env vars MYQ_USER and/or MYQ_PASS are not defined!');
-  process.exit(1);
+for (const reqEnv of [
+  'SMTP_USER',
+  'SMTP_PASS',
+  'SMTP_HOST',
+  'SMTP_FROM',
+  'SMTP_TO',
+  'MYQ_USER',
+  'MYQ_PASS'
+]) {
+  if (!process.env[reqEnv]) {
+    console.log(`ERROR: Env var ${reqEnv} is not defined.`);
+    process.exit(1);
+  }
 }
 
 const openDoors = {};
 
 const myQ = new MQ(process.env.MYQ_USER, process.env.MYQ_PASS);
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT || 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.SMTP_USER, // generated ethereal user
+    pass: process.env.SMTP_PASS // generated ethereal password
+  }
+});
 
 const refreshState = (myQ) => {
   myQ.refreshDevices().then(() => {
@@ -30,9 +51,14 @@ const refreshState = (myQ) => {
           const openAgeMinutes = Math.round(((new Date()) - openDoors[device.serial_number].openAt) / 60 / 1000);
           console.info('Garage door ', device.serial_number, ' has been open for ', openAgeMinutes, ' minutes.');
           if (openAgeMinutes > (process.env.MYQ_MAX_OPEN_DURATION_MINUTES || 10)) {
-            console.error('THIS IS A PROBLEM!');
-          } else {
-            console.error('Not yet a problem.');
+            transporter.sendMail({
+              from: process.env.SMTP_FROM,
+              to: process.env.SMTP_TO,
+              subject: 'Garage Alert',
+              text: 'Door is open'
+            }).then((info) => {
+              console.log('Email sent: %s', info.messageId);
+            });
           }
         }
       } else {
@@ -49,6 +75,15 @@ const refreshState = (myQ) => {
 
 // initial run
 refreshState(myQ);
+
+transporter.sendMail({
+  from: process.env.SMTP_FROM,
+  to: process.env.SMTP_TO,
+  subject: 'Garage Alert',
+  text: 'Starting Up'
+}).then((info) => {
+  console.log('Email sent: %s', info.messageId);
+});
 
 // interval run
 setInterval(() => {
